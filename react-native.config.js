60b2ee3654eba64a6e24d3e1ad36375be1de32ec
DanielPaz <thothowe@gmail.com>
2026-07-02 12:32:48 -0300

    chore: configure native fonts assets

---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+/**
+ * Configuração de assets nativos do React Native CLI.
+ *
+ * Sempre que adicionar/remover arquivos .ttf ou .otf em assets/fonts,
+ * rode: yarn link:fonts
+ */
+module.exports = {
+  assets: ['./assets/fonts/'],
+};
